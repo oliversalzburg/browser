ab890b46d1fba57c0cbf0f0bd0791b539419bcd8
Oliver Salzburg <oliver.salzburg@gmail.com>
2023-11-14 11:40:34 +0100

    feat: Modernize

---
+++ .eslintrc.cjs
@@ -0,0 +1,62 @@
+"use strict";
+
+module.exports = {
+  root: true,
+
+  env: {
+    node: true,
+    es2022: true,
+  },
+
+  ignorePatterns: ["output/*"],
+  overrides: [
+    {
+      files: ["*.cjs"],
+      extends: ["eslint:recommended"],
+      parserOptions: {
+        sourceType: "commonjs",
+      },
+    },
+    {
+      files: ["*.js", "*.mjs"],
+      extends: ["eslint:recommended"],
+      parserOptions: {
+        sourceType: "module",
+      },
+    },
+    {
+      files: ["*.cts", "*.mts", "*.ts"],
+      extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/strict-type-checked",
+        "plugin:@typescript-eslint/stylistic-type-checked",
+      ],
+      parserOptions: {
+        project: ["./tsconfig.json"],
+        tsconfigRootDir: __dirname,
+      },
+      plugins: ["@typescript-eslint"],
+      rules: {
+        "@typescript-eslint/array-type": ["error", { default: "generic" }],
+        "@typescript-eslint/no-explicit-any": [
+          "error",
+          {
+            ignoreRestArgs: true,
+          },
+        ],
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": ["error", { args: "none" }],
+        "@typescript-eslint/no-var-requires": "off",
+      },
+    },
+  ],
+  rules: {
+    "consistent-return": "error",
+    "no-else-return": "error",
+    "no-unused-expressions": "warn",
+    "no-use-before-define": "error",
+    eqeqeq: "error",
+    quotes: "warn",
+    strict: ["error", "global"],
+  },
+};
