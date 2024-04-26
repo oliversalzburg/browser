736f48b405d0117bd14f3a11c0389dfcc2da744c
Oliver Salzburg <oliver.salzburg@gmail.com>
2024-04-26 13:15:37 +0200

    fix: Incomplete ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -36,10 +36,15 @@ export default tseslint.config(
   },
   {
     files: ["**/*.cts", "**/*.mts", "**/*.ts"],
-    extends: [eslint.configs.recommended, ...tseslint.configs.recommendedTypeChecked],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
     languageOptions: {
       parserOptions: {
         project: true,
+        tsconfigRootDir: import.meta.dirname,
       },
     },
     rules: {
